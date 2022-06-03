eb0051d84c09c6083d8cce9f2d593acc2e28f03b
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2022-06-03 02:59:55 -0700

    html and wriring up is done

---
+++ istriangle.js
@@ -0,0 +1,12 @@
+const inputs=document.querySelectorAll('.angle-input')
+const trianglebutton=document.querySelector('#btn-triangle')
+const output=document.querySelector('#showoutput')
+
+
+
+
+
+
+
+
+
