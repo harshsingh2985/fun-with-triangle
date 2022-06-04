1a609866d58e6bcd4acc8035c7aedad68a4a8cd5
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2022-06-04 09:41:08 -0700

    update:css

---
+++ trianglequiz.js
@@ -0,0 +1,8 @@
+const form=document.querySelector(".quiz-form")
+
+const submitbtn=document.querySelector("#submit")
+
+const output=document.querySelector("#output-shn")
+
+const correctAnswers = ['90°', 'right angled'];
+
